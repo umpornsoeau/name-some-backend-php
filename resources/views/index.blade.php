35826cf7729177d7c1082ac32296472e30460f80
umpornsoeau <umpornsoeau@protonmail.com>
2020-11-10 04:36:34 +0000

    update landing page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,26 @@
-<html>
-    <body>
-        <h1>Hello, {{ $appname }}</h1>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <title>NameSome - Backend - PHP</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/chota.min.css">
+  <link rel="stylesheet" href="/app.css">
+  <link rel="icon" type="image/png" href="favicon.png" />
+</head>
+<body>
+<div id="top" class="container">
+<div class="is-center">
+    <h3>A <strong>NameSome-Backend</strong> with PHP + Laravel/Lumen</h3>
+</div>
+<nav class="tabs is-center">
+    <a href="#features">features</a>
+    <a href="#start">start</a>
+    <a href="#docs">docs</a>
+    <a href="https://github.com/jenil/chota">GitHub</a>
+</nav>
+</div>
+</body>
 </html>
 
